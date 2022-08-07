96717c2c2f3313f258a23a71701e8a4400b95e49
pcl1203 <96214370+pcl1203@users.noreply.github.com>
2022-08-07 20:04:08 +0800

    Added score system and display score on game over screen when died

--- Assets/Scripts/GameOverHandler.cs
+++ Assets/Scripts/GameOverHandler.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOverHandler : MonoBehaviour
 {
+   [SerializeField] private TMP_Text gameText;
    [SerializeField] private GameObject gameOverDisplay;
    [SerializeField] private AsteroidSpawner asteroidSpawner;
+   [SerializeField] private ScoreSystem scoreSystem;
    public void PlayAgain()
    {
       SceneManager.LoadScene(1);
@@ -19,5 +22,8 @@ public class GameOverHandler : MonoBehaviour
    {
       asteroidSpawner.enabled = false;
       gameOverDisplay.gameObject.SetActive(true);
+
+      int finalScore = scoreSystem.EndTimer();
+      gameText.text = $"Your Score: {finalScore}";
    }
 }
