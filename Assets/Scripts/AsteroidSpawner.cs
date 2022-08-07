6f0d031ae27cb2096ae22b4cb79d0012f8398bed
pcl1203 <96214370+pcl1203@users.noreply.github.com>
2022-08-07 17:21:54 +0800

    Added Asteroid Spawn action with random movement ang position

---
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+   [SerializeField] private GameObject[] asteroidPrefabs;
+   [SerializeField] private float secondsBetweenAsteroids = 1.5f;
+   [SerializeField] private Vector2 forceRange;
+
+   private Camera mainCamera;
+   private float timer;
+
+   private void Start()
+   {
+      mainCamera = Camera.main;
+
+   }
+   // Update is called once per frame
+   void Update()
+   {
+      timer -= Time.deltaTime;
+
+      if (timer <= 0) {
+         SpawnAsteroid();
+         timer += secondsBetweenAsteroids;
+      }
+   }
+
+   private void SpawnAsteroid()
+   {
+      int side = Random.Range(0, 4);
+
+      Vector2 spawnPoint = Vector2.zero;
+      Vector2 direction = Vector2.zero;
+
+      switch (side)
+      {
+         case 0: // Left part of the screen
+            spawnPoint.x = 0;
+            spawnPoint.y = Random.value;
+            direction = new Vector2(1f, Random.Range(-1f, 1f));
+            break;         
+         case 1:
+            // right part of the screen
+            spawnPoint.x = 1;
+            spawnPoint.y = Random.value;
+            direction = new Vector2(-1f, Random.Range(-1f, 1f));
+            break;         
+         case 2: // bottom part of the screen
+            spawnPoint.x = Random.value;
+            spawnPoint.y = 0;
+            direction = new Vector2(Random.Range(-1f, 1f), 1f);
+            break;        
+         case 3:// top part of the screen
+            spawnPoint.x = Random.value;
+            spawnPoint.y = 1;
+            direction = new Vector2(Random.Range(-1f, 1f), -1f);
+            break;
+      }
+
+      Vector3 worldSpawnPoint = mainCamera.ViewportToWorldPoint(spawnPoint);
+      worldSpawnPoint.z = 0;
+
+      GameObject selectedAsteroid = asteroidPrefabs[Random.Range(0, asteroidPrefabs.Length)];
+
+      GameObject asteroidInstance = Instantiate(
+         selectedAsteroid, 
+         worldSpawnPoint, 
+         Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)));
+
+      Rigidbody rb = asteroidInstance.GetComponent<Rigidbody>();
+      rb.velocity = direction.normalized * Random.Range(forceRange.x, forceRange.y);
+   }
+}
